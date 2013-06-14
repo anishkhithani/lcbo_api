7b2c4378ccb6ae7d085f0c47b1ed7d90a94284b8
Anish Khithani <anish.khithani@gmail.com>
2013-06-14 17:30:54 -0400

    finished first def for a full index by search query

--- html_generator.rb
+++ html_generator.rb
@@ -4,36 +4,39 @@ require "open-uri"
 class HtmlGenerator
 
   def index(search_text) #searches database
-    products = ######## ("http://lcboapi.com/products?q=#{seach_text}")
+    products = retreive_data("http://lcboapi.com/products?q=#{search_text}")
+
+    print_header
+
+    puts "<h1> All yo booze son </h1>"
+
+    products.each do |product|
+      puts "<div class = 'product'>"
+        puts "<h2> #{product['name']} </h2>"
+        puts "<img src= '#{product['image_thumb_url']}' class='product_img' />"
+        puts "<ul class = 'product_data'>"
+          puts "<li> #{product['id']}</li>"
+          puts "<li> #{product['regular_price_in_cents']}</li>"
+          if product['price_in_cents'] != product['regular_price_in_cents'] 
+            puts "<li> On sale! #{product['price_in_cents']}</li>"
+          else
+          puts nil
+          end
+          puts product[""]
+          puts product[""]
+          puts product[""]
+        puts "</ul>"
+      puts "</div>"
+    end  
   end
 
-  def product(product_id) #shows specific product all product info
-  end
-
-  def drunk
-  end
 
 
+  # def product(product_id) #shows specific product all product info
+  # end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  # def drunk
+  # end
 
 
   def retreive_data(url)
@@ -43,14 +46,27 @@ class HtmlGenerator
     #takes the JSON formatted text file and parses putting it in a ruby-readble file
     #this new file will be a hash>array>hash
     parsed_response = JSON.parse(raw_response)
+
+    #goes into the first has to retreive the key "result" which houses all the product info details
+    #each product is one index of an array, within each product the key/value pairs are product attributes
+    result = parsed_response["result"]
   end
 
   def print_header
+    puts "<!DOCTYPE html>"
     puts "<html>"
     puts "<head>"
     puts "Dane's Dranky Dranks"
-    puts "<link type = "css" rel="stylesheet" href="reset.css" />"
-    puts "<link type = "css" rel = "stylesheet" href = "style.css" />"
+    puts "<link type = 'css' rel='stylesheet' href='reset.css' />"
+    puts "<link type = 'css' rel = 'stylesheet' href = 'style.css' />"
     puts "</head>"
+    puts "<body>"
+  end
+
+  def print_footer
+    puts "</body>"
+    puts "</html>"
+  end
+end
 
 
---
+++ router.rb
@@ -0,0 +1,27 @@
+require_relative "html_generator"
+
+if ARGV.empty?
+  puts "Usage: ruby router.rb [action]"
+  puts "Actions include:"
+  puts "index 'query'"
+  puts "product 'product id'"
+  #add more instructions
+
+else
+  action = ARGV[0]
+  generator = HtmlGenerator.new
+
+    if action == "index"
+      search_text = ARGV[1]
+      generator.index(search_text)
+
+    elsif action == "product"
+      product_id = ARGV[1]
+      generator.product(product_id)
+    end
+end
+
+
+      
+
+
